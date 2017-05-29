10822233371344dcda0956dd7a46c4039dd86b4d
Huan Wang <huhwang@mines.edu>
2017-05-29 02:47:52 -0600

    mod done for ds original version.

--- modinfo.lua
+++ modinfo.lua
@@ -9,6 +9,9 @@ dont_starve_compatible = true
 reign_of_giants_compatible = false
 shipwrecked_compatible = false
 
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
 -- dst_compatible = true
 -- all_clients_require_mod = false
 -- client_only_mod = true
